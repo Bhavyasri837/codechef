13771e054aa68e67ca1de98e83dca80c616a8c2e
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-14 15:59:46 +0530

    Create algomaniac.java

---
+++ algomaniac.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class algomaniac
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		if(X==17)
+		{
+		    System.out.println("YAY");
+		}
+		else{
+		    System.out.println("NO");
+		}
+
+	}
+}
