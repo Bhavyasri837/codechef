607b8404e51ad01f8389d1f15ffe4a49770d4a24
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-14 15:53:27 +0530

    Create valenties.java

---
+++ valenties.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class valenties
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n==121)
+		{
+		    System.out.println("likely");
+		}
+		else 
+		{
+		    System.out.println("unlikely");
+		}
+
+	}
+}
