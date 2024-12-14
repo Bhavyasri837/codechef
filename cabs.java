25e53961c9acdfa6f62361932d588f814f918771
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-14 15:46:08 +0530

    Add files via upload

---
+++ cabs.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class cabs {
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    if(x<y)
+		    System.out.println("FIRST");
+		    else if(x>y)
+		    System.out.println("SECOND");
+		    else if(x==y)
+		    System.out.println("ANY");
+		}
+
+	}
+}
+
+
