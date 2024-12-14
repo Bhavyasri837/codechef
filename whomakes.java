e5e819f80208e224f9e6b7a722fe80c74908d938
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-14 15:48:37 +0530

    Add files via upload

---
+++ whomakes.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class whomakes {
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if(a<=b)
+		System.out.println("TYRO");
+		else
+		System.out.println("DOM");
+
+	}
+}
+
