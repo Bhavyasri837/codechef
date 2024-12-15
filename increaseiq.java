9f3a8def68d12619cc4b0b3b9e8c0eb190d0458f
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-15 11:12:10 +0530

    Create increaseiq.java

---
+++ increaseiq.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class increaeiq
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		if((7+x)>170)
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+
+	}
+}
