16776e9af8d58d65b5dd39401e93b31c886fe8d2
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-19 00:28:26 +0530

    Create additionofvariables.java

---
+++ additionofvariables.java
@@ -0,0 +1,14 @@
+class additionofvariables
+{
+	public static void main (String[] args)
+	{
+	   // Declare a and 23 to it.
+	   int a=23;
+	   int b=20;
+	   System.out.println(a+b);
+	   
+	   // Declare b and assign 20 to it.
+
+
+	}
+}
