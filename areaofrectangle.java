00a1ff539eda41027b82459d99f905a6a5450ccd
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-19 00:27:31 +0530

    Create areaofrectangle.java

---
+++ areaofrectangle.java
@@ -0,0 +1,11 @@
+class areaofrectangle
+{
+	public static void main (String[] args)
+	{
+	   // Update the blanks in the code below
+        int length = 45;
+        int width = 76;
+        int area = length * width;
+        System.out.println("The Area of the given rectangle is " + area);
+	}
+}
