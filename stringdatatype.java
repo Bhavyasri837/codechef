99d57b7d3c18257156e3aaf68756bd02d1206b65
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-19 00:25:59 +0530

    Create stringdatatype.java

---
+++ stringdatatype.java
@@ -0,0 +1,10 @@
+class stringdatatype
+{
+	public static void main (String[] args)
+	{
+        // Declare variables, join them and output them
+        String a="Code";
+        String b="Chef";
+        System.out.println(a+b);
+	}
+}
