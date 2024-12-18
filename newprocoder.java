748fc5f9278b227e81bea9dd8e1c0a0154ac1673
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-19 00:21:23 +0530

    Create newprocoder.java

---
+++ newprocoder.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class newprocoder
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		float f=n/2f;
+		if(m>=f)
+		System.out.println("NEWBIE");
+		else
+		System.out.println("PRO");
+
+	}
+}
