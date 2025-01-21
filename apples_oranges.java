029f55529d8819d4b181a435630ee2d55a123eb6
Bhavyasri837 <nsbhavya83@gmail.com>
2025-01-21 23:30:47 +0530

    Create apples_oranges.java

---
+++ apples_oranges.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class apples_oranges
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if((a+b)<=x)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+
+	}
+}
